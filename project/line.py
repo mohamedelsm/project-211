93293c77eaa91073740b50ff5fa68e240d3a56c4
Mohamed Elsamadouny <mhsamadouny@gmail.com>
2023-03-21 12:12:17 -0400

    closest-color implementation

--- project/line.py
+++ project/line.py
@@ -1,4 +1,5 @@
 from utils.brick import BP, Motor, TouchSensor, EV3ColorSensor, wait_ready_sensors, reset_brick 
+import numpy as np
 import time
 
 MOTORL = Motor("A") 
@@ -14,6 +15,19 @@ ORIENTTODEG = RB/RW
 # set motor limits
 print("sensors initializing")
 wait_ready_sensors()
+
+
+def closest(color):
+    colors = np.array([[175, 20, 12],[190, 36, 18],[200, 128, 22],[30, 68, 20],[22, 22, 33],[120,14,28]])
+    colors_name = ["RED", "ORANGE","YELLOW","GREEN","BLUE","PURPLE"]
+    color = np.array(color)
+
+    distances = np.sqrt(np.sum((colors-color)**2,axis=1))
+    index_of_smallest = np.where(distances==np.amin(distances))
+    smallest_distance = colors_name[index_of_smallest[0][0]]
+    return smallest_distance 
+
+
 # main entry point
 try:
     # motor initialization
@@ -26,19 +40,17 @@ try:
     while True:
         rgb = COLOR_SENSOR.get_rgb()
         print(rgb)
-        R=rgb[0]
-        G=rgb[1]
-        B=rgb[2]
-        
-        if R in range(110,190) and G in range(10,28) and B in range(10,16):
+        color = closest(rgb)
+
+        if color == "RED":
             print("red")
             MOTORL.set_power(-15)
             MOTORR.set_power(70)
-        elif R in range(15,30) and G in range(15,30) and B in range(25,40):
+        elif color == "BLUE":
             print("blue")
             MOTORL.set_power(70)
             MOTORR.set_power(-15)
-        elif R in range(15,45) and G in range(40,75) and B in range(5,25):
+        elif color == "GREEN":
             print("green")
             MOTORL.set_power(0)
             MOTORR.set_power(0)
@@ -47,8 +59,10 @@ try:
             MOTORR.set_power(20)
         
         time.sleep(0.1)
-        
+
         # do smth with the rgb values
+
+        
             
 except KeyboardInterrupt:
     print("Ending by keyboard interrupt")

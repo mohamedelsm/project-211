1f93e9e98d65c465077138f6afa78bb9e881a088
A-NAMI <ahmed.nami@mail.mcgill.ca>
2023-04-11 23:24:50 -0400

    Final updates and fixes

--- project/line.py
+++ project/line.py
@@ -19,9 +19,9 @@ def side_check_color(rgb):
     means = [[0.98269282, 0.166543148, 0.079804332], [0.956722739, 0.279694185, 0.075963383], 
              [0.736488044, 0.672059319, 0.075039654], [0.313354802, 0.927045185, 0.202401399],
              [0.368746318, 0.608397841, 0.698980325], [0.963167792, 0.175536085, 0.201083857]]
-    std_dev = [[0.004002433, 0.016609388, 0.01300176], [0.00668939, 0.023818665, 0.008397344],
-               [0.00948927, 0.009867564, 0.010167148], [0.027895648, 0.014312723, 0.021143952], 
-               [0.05831743, 0.019102171, 0.039416204], [0.018729752, 0.035987929, 0.027576094]]
+    std_dev = [[0.006002433, 0.018609388, 0.01800176], [0.00668939, 0.023818665, 0.008397344],
+               [0.02448927, 0.022867564, 0.025167148], [0.027895648, 0.014312723, 0.021143952], 
+               [0.06231743, 0.022102171, 0.044416204], [0.018729752, 0.035987929, 0.027576094]]
     
     color_names = ["RED", "ORANGE","YELLOW","GREEN","BLUE","PURPLE"]
 
@@ -44,20 +44,20 @@ def side_check_color(rgb):
         diffG=(mG-g)/sG + 0.05
         diffB=(mB-b)/sB + 0.05
         std_dist=math.sqrt(diffR**2 + diffG**2 + diffB**2)
-        if std_dist < 5:
+        if std_dist < 4:
             return color_names[i]
             
     return None
 
 def check_color(rgb):
-    means = [[0.987566201, 0.133367577, 0.080584644], [0.96937965, 0.2273774, 0.08586806], 
+    means = [[0.987566201, 0.133367577, 0.080584644],
              [0.82395664, 0.55897973, 0.09190268], [0.407351948, 0.884058889, 0.227519999],
-             [0.454594521, 0.480832567, 0.748704212], [0.967152296, 0.142217565, 0.209920143]]
-    std_dev = [[0.00334054, 0.013688487, 0.015294446], [0.01000573, 0.02504622, 0.02241218],
+             [0.454594521, 0.480832567, 0.748704212]]
+    std_dev = [[0.01534054, 0.015688487, 0.025294446], 
                [0.00642989, 0.00992374, 0.00716117], [0.016844783, 0.010902598, 0.01834428], 
-               [0.025688715, 0.022795108, 0.020173633], [0.003742952, 0.014803877,	0.0095453699]]
+               [0.028688715, 0.025795108, 0.023173633]]
     
-    color_names = ["RED", "ORANGE","YELLOW","GREEN","BLUE","PURPLE"]
+    color_names = ["RED","YELLOW","GREEN","BLUE"]
 
     for i in range(len(means)):
         mean = means[i]
@@ -87,20 +87,20 @@ def orient(prev_color):
     if prev_color=='BLUE' or prev_color == None:
         MOTORR.set_power(-20)
         MOTORL.set_power(20)
-        time.sleep(1)
+        time.sleep(1.2)
         MOTORR.set_power(0)
         MOTORL.set_power(0)
     
     elif prev_color == 'RED':
-        MOTORL.set_power(-20)
-        MOTORR.set_power(20)
+        MOTORL.set_power(-15)
+        MOTORR.set_power(15)
         time.sleep(0.5)
         MOTORR.set_power(0)
         MOTORL.set_power(0)
         
     MOTORR.set_power(20)
     MOTORL.set_power(20)
-    time.sleep(0.75)
+    time.sleep(0.5)
     MOTORR.set_power(0)
     MOTORL.set_power(0)
     #time.sleep(2)
@@ -128,26 +128,26 @@ def rotate_platform(zone_color):
     if zone_color=="RED":
         angle=0
     elif zone_color=="ORANGE":
-        angle=70
+        angle=60
     elif zone_color=="YELLOW":
         angle=120
     elif zone_color=="GREEN":
-        angle=195
+        angle=180
     elif zone_color=="BLUE":
         angle=240
     elif zone_color=="PURPLE":
         angle=300
     MOTORPLAT.reset_encoder()
-    MOTORPLAT.set_limits(30)
+    MOTORPLAT.set_limits(25)
     MOTORPLAT.set_position(angle)
-    time.sleep(1.25)
+    time.sleep(1.45)
     kick_block()
     time.sleep(1.15)
     MOTORPLAT.set_position(0)
 
 def kick_block():
     reset_motor()
-    MOTORKICK.set_limits(35)
+    MOTORKICK.set_limits(50)
     MOTORKICK.set_position(135)
     time.sleep(1)
     MOTORKICK.set_position(0)
@@ -193,25 +193,16 @@ def go_back_on_track():
             MOTORL.set_power(30)
             MOTORR.set_power(-30)
             return
+        time.sleep(0.1)
+    
 
-def do_360(speed):
+def turn_start(speed):
     print("doing 360")
     rgb = COLOR_SENSOR.get_rgb()
     color = check_color(rgb)
     while color != "BLUE":
         if color == "GREEN":
-            print("green")
-            rgb = COLOR_SENSOR.get_rgb()
-            color = check_color(rgb)
-            while color == "GREEN":
-                rgb = COLOR_SENSOR.get_rgb()
-                color = check_color(rgb)
-                MOTORL.set_power(-1 * speed)
-                MOTORR.set_power(speed)
-                time.sleep(0.1)
-            print("done green")
-            return    
-        #go_backwards(0.5)
+            go_backwards(0.5)   
         rgb = COLOR_SENSOR.get_rgb()
         color = check_color(rgb)
         MOTORL.set_power(-1 * speed)
@@ -239,11 +230,11 @@ def return_to_loading():
             MOTORR.set_power(30)
         elif color == "GREEN":
             if prev_color == "RED":
-                MOTORL.set_power(30)
-                MOTORR.set_power(-5)
+                MOTORL.set_power(45)
+                MOTORR.set_power(5)
             elif prev_color == "BLUE":
-                MOTORL.set_power(-5)
-                MOTORR.set_power(30)
+                MOTORL.set_power(5)
+                MOTORR.set_power(45)
             #time.sleep(0.5)    
 #             MOTORL.set_power(30)
 #             MOTORR.set_power(30)
@@ -288,38 +279,36 @@ def deliver_blocks():
                 orient(prev_color) #function by matt
                 zone_color = find_zone()
                 move_out_of_zone(zone_color)
+                wiggle(3)
                 blocks_delivered += 1
                 print("blocks delivered = " + str(blocks_delivered))
                 if blocks_delivered != 6:
                     go_back_on_track()
             else:
-                MOTORL.set_power(25)
-                MOTORR.set_power(25)
+                MOTORL.set_power(15)
+                MOTORR.set_power(35)
             time.sleep(0.1)
             
 def turn_end(speed):
     print("turning at the end")
-    # turn till you read blue
-    rgb = COLOR_SENSOR.get_rgb()
-    color = check_color(rgb)
-    while color != "BLUE":
-        rgb = COLOR_SENSOR.get_rgb()
-        color = check_color(rgb)
-        MOTORL.set_power(-1 * speed)
-        MOTORR.set_power(speed)
-        time.sleep(0.1)
-        
     #go back
     MOTORL.set_power(-30)
     MOTORR.set_power(-10)
-    time.sleep(1)
     
     # turn till it sees red
     rgb = COLOR_SENSOR.get_rgb()
     color = check_color(rgb)
     while color != "RED":
+            
         rgb = COLOR_SENSOR.get_rgb()
         color = check_color(rgb)
+        
+        if color == "GREEN":
+            go_forward(0.25)
+            MOTORR.set_power(-10)
+            MOTORL.set_power(25)
+            time.sleep(0.8) 
+            
         MOTORL.set_power(-1 * speed)
         MOTORR.set_power(speed)
         time.sleep(0.1)
@@ -339,7 +328,24 @@ def go_backwards(t):
     MOTORR.set_power(-25)
     time.sleep(t)
     
+def wiggle(t):
+    for i in range(t):
+        MOTORL.set_power(-20)
+        MOTORR.set_power(20)
+        time.sleep(0.25)
+        MOTORL.set_power(20)
+        MOTORR.set_power(-20)
+        time.sleep(0.25)
 
+def oopsie_unload():
+    angle=0
+    for i in range(6):
+        kick_block()
+        time.sleep(0.2)
+        angle+=60
+        MOTORPLAT.set_position(angle)
+        time.sleep(0.7)
+        
 try:
     # set motor limits
     print("sensors initializing")
@@ -355,12 +361,14 @@ try:
         if not TOUCH_SENSOR.is_pressed() and is_pressed == True:
             print("button pressed")
             is_pressed = False
+            time.sleep(0.1)
             deliver_blocks()
-            go_forward(1.5)
-            turn_end(30)
+            go_forward(1.8)
+            oopsie_unload()
+            turn_end(25)
             return_to_loading()
-            fggo_forward(1.75)
-            do_360(25)
+            go_forward(0.4)
+            turn_start(25)
             MOTORL.set_power(20)
             MOTORR.set_power(-20)
             time.sleep(1.5)
